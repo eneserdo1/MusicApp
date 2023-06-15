a6393069ad9575202defae09c89f2a8994dbc369
eneserdogan <eneserdogan.66@outlook.com>
2023-06-15 22:23:41 +0300

    movie grid list fragment added

---
+++ app/src/main/java/com/app/movieapp/ui/movieGridList/fragment/MovieGridListFragment.kt
@@ -0,0 +1,29 @@
+package com.app.movieapp.ui.movieGridList.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.app.movieapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieGridListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_grid_list, container, false)
+    }
+
+    companion object {
+        fun newInstance() = MovieGridListFragment()
+    }
+}
